e295c3d10b3757fafc710fc469989c1ffb4cc077
이경용 <sleekydz86@icloud.com>
2024-01-14 18:15:25 +0900

    프로젝트 셋팅
    JPA, 트랜잭션, 락 학습을 위한 업로드

---
+++ JpaRackTransactional/src/main/java/com/rack/jpatransactional/JpaRackTransactionalApplication.java
@@ -0,0 +1,13 @@
+package com.rack.jpatransactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRackTransactionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaRackTransactionalApplication.class, args);
+    }
+
+}
---
+++ JpaRackTransactional/src/test/java/com/rack/jpatransactional/JpaRackTransactionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rack.jpatransactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRackTransactionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
