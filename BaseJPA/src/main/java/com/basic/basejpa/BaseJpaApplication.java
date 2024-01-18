f8b802cf7f7dc5a7e4a78ef87d308503c4e1f67f
이경용 <sleekydz86@icloud.com>
2024-01-19 00:26:07 +0900

    JPA베이직 셋팅
    JPA베이직 셋팅

---
+++ BaseJPA/src/main/java/com/basic/basejpa/BaseJpaApplication.java
@@ -0,0 +1,13 @@
+package com.basic.basejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseJpaApplication.class, args);
+    }
+
+}
---
+++ BaseJPA/src/test/java/com/basic/basejpa/BaseJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.basejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
